9631e46a21841796e09f2e0bd997434c7fe6aa16
Henrique Dias <henriquejnd@proton.me>
2023-10-11 17:30:08 -0300

    Titulo, resultado, slider

--- passwordGeneratorApp/src/App.jsx
+++ passwordGeneratorApp/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  return(
+    <div class='App'>
+      <h1>Password Generator</h1>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class='Resultado'>
+        <p>P4$5W0rD!</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div class='Configs'>
+        <div id='length'>
+          <div id='valores'>
+          <label htmlFor="scrollBar">Character Length</label>
+          <span id='rangeResult'>0</span>
+          </div>
+          <input type="range" id='scrollBar' />
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
