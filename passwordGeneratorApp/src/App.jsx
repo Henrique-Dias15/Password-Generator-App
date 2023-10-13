61e7c317ec9c846ebc7f3f479beb77759cf811ff
GabsRMartins <gabsmartins262@gmail.com>
2023-10-13 12:15:23 -0300

    incluindo a barra de strength e o botão de gerar

--- passwordGeneratorApp/src/App.jsx
+++ passwordGeneratorApp/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <label htmlFor="scrollBar">Character Length</label>
           <span id='rangeResult'>0</span>
           </div>
-          <input type="range" id='scrollBar' />
+          <input type="range"  name="slider" id="slider" />
         </div>
 
         <div id='checkBoxes'>
@@ -35,6 +35,20 @@ function App() {
             <input type='checkbox' id='includeSymbols'/>
             <label htmlFor="includeSymbols">Include Symbols</label>
           </div>
+        </div>
+        <div id='strength'>
+          <h2 htmlFor='STRENGTH'>STRENGTH</h2>
+          <div class="strengthlvl">
+            <p class="fs-2" id="levelTxt">WEAK</p>
+            <div id="bar1" class="bars bar1"></div>
+            <div id="bar2" class="bars bar2"></div>
+            <div id="bar3" class="bars bar3"></div>
+            <div id="bar4" class="bars bar4"></div>
+
+          </div>
+        </div>
+        <div class="generateBtn">
+        <button id="generateBtn" class="button">GENERATE<svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path fill="#24232C" d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z"/></svg></button> 
         </div>
       </div>
     </div>
